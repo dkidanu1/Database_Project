f60a5600ef32b0b43699a7d4ba5e12f80113cc93
dkidanu1 <dkidanu@gmail.com>
2021-02-17 06:07:05 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $(".collapsible").collapsible();
-});
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    let sidenavs = document.querySelectorAll(".sidenav");
-    let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
-    let collapsibles = document.querySelectorAll(".collapsible");
-    let collapsiblesInstance = M.Collapsible.init(collapsibles);
+    $(".tooltipped").tooltip();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
